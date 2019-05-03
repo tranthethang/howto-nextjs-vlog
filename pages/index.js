3bc9bac7c6cf57ed5e1cdc50f570eed66bdace25
thangtt-d6 <thangtt@rikkeisoft.com>
2019-05-03 17:19:19 +0700

    deploy to ZEIT with now

--- pages/index.js
+++ pages/index.js
@@ -9,42 +9,45 @@ function getPosts() {
   ]
 }
 
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+)
 export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
         {getPosts().map(post => (
-          <li key={post.id}>
-            <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
+          <PostLink key={post.id} post={post} />
         ))}
       </ul>
-      <style jsx>
-        h1, a {
-            font-family: "Arial";
-        }
-
-        ul {
-            padding: 0;
-        }
-
-        li {
-            list-style: none;
-            margin: 5px 0;
-        }
-
+      <style jsx>{`
+        h1,
         a {
-            text-decoration: none;
-            color: blue;
+          font-family: 'Arial';
         }
-
-        a:hover {
-            opacity: 0.6;
+        ul {
+          padding: 0;
         }
-        </style>
+      `}</style>
     </Layout>
   )
 }
